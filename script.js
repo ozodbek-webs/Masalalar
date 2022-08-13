a14e54234122195b33dc216a6878c4860c614640
ozodbek-web <ozodbektursunboyev77@gmail.com>
2022-08-13 23:04:04 +0500

    first repo

---
+++ script.js
@@ -0,0 +1,340 @@
+// // 1-misol
+// var l = prompt("1-misol uchun uzunlikni sm da kiriting!")
+// console.log(l/100+" "+"metr")
+// // 2-misol
+// var m=prompt(
+//     "2-misol uchun son kiriting"
+// )
+// console.log(
+//     m/1000+" "+"tonna"
+// )
+// // 3-misol
+// var a=prompt("3-misol uchun 1-sonni kiriting")
+// var b=prompt("3-misol uchun 2-sonni kiriting")
+// console.log(
+//     a/b+" "+"marta"
+// )
+
+// // 4-misol
+// var f=prompt("4-misol uchun son kiriting")
+// var g=f%10
+// var j=(((a%100)-g)/10)
+// console.log(
+//     j+" "+"ta 10 xonali",f%10+" "+"ta 1 xonali"
+// )
+// // 5-misol
+
+// var a=prompt("5-misol uchun son kiriting")
+// var b=a%10
+// var c=(((a%100)-b)/10)
+// console.log(
+//     b+c+" "+"qo`shilmasi"
+// )
+// // 6-misol
+
+// var a=prompt("6-misol uchun son kiriting")
+// var b=a%10
+// var c=(((a%100)-b)/10)
+// console.log(
+//     b*c+" "+"ko`paytmasi"
+// )
+
+// // 7-misol
+
+// var a = prompt( "7-misol uchun iltimos 999 dan katta son kiriting" )
+// var c=a%110;
+// var d=((a-c))
+// var e = ((d/10)/10);
+// console.log("Unda "+e+" ta 100 xonali son bor!")
+
+// // 8-misol
+
+// console.log(Date())
+
+// // If Else
+
+
+
+// // 1-misol
+
+// var a =prompt("Iltimos butun son kiriting!")
+// var b=a*1
+// if(b > 0){
+//     console.log("Bu musbat son")
+// }else (
+//     console.log("Bu manfiy son")
+// )
+
+
+
+// // 2-misol
+
+// var a =prompt("Iltimos juft son kiriting!")
+// var b=a*1
+// var c=b%2
+// if(c==0){
+//     console.log("Bu juft son")
+// }else (
+//     console.log("Bu toq son")
+// )
+
+// // 3-misol
+
+// var a =prompt("Iltimos juft son kiriting!")
+// var b=a*1
+// var c=b%2
+// if(c==0){
+//     console.log("Bu juft son")
+// }else (
+//     console.log("Bu toq son")
+// )
+
+
+
+// // 4-misol
+
+
+
+
+// var a =prompt("Iltimos butun son kiriting!")
+// var c =prompt("Iltimos butun son kiriting!")
+// var b=a*1
+// var m=c*1
+// if(b > 0 && m > 0){
+//     console.log("True")
+// }else (
+//     console.log("False")
+// )
+
+
+// // 4-misol
+
+// var a =prompt("Iltimos 1-butun sonni kiriting!")
+// var b =prompt("Iltimos 2-butun sonni kiriting!")
+// var c =prompt("Iltimos 3-butun sonni kiriting!")
+// var m=a*1
+// var n=b*1
+// var o=c*1
+// if(m > n && n > c){
+//     console.log("True")
+// }else (
+//     console.log("False")
+// )
+
+
+// // 5-misol
+
+// var a =prompt("Iltimos 1-butun sonni kiriting!")
+// var b =prompt("Iltimos 2-butun sonni kiriting!")
+// var c =prompt("Iltimos 3-butun sonni kiriting!")
+// var m=a*1
+// var n=b*1
+// var o=c*1
+// if(m > n && n > c){
+//     console.log("True")
+// }else (
+//     console.log("False")
+// )
+
+
+
+// // 6-misol
+
+
+
+// var a =prompt("Iltimos butun son kiriting!")
+// var c =prompt("Iltimos butun son kiriting!")
+// var b=a*1
+// var m=c*1
+// var n=b%2
+// var o=m%2
+// if(n==0 && o==0){
+//     console.log("Bular musbat son")
+// }else (
+//     console.log("Bular manfiy son")
+// )
+
+
+
+
+// // 7-misol
+
+
+
+// var a =prompt("Iltimos 1-butun sonni kiriting!")
+// var c =prompt("Iltimos 2-butun sonni kiriting!")
+// var p =prompt("Iltimos 3-butun sonni kiriting!")
+// var l =prompt("Iltimos 4-butun sonni kiriting!")
+// var b=a*1
+// var m=c*1
+// var k=p*1
+// var i=l*1
+
+// var n=b%2
+// var o=m%2
+// var t=k%2
+// var e=i%2
+// if(n==0 && o==0 && t==0 && e==0){
+//     console.log("Bu true")
+// }else (
+//     console.log("Bu false")
+// )
+
+
+
+
+
+
+// // 9-misol
+
+
+// var a =prompt("Iltimos 1-butun sonni kiriting!")
+// var c =prompt("Iltimos 2-butun sonni kiriting!")
+// var p =prompt("Iltimos 3-butun sonni kiriting!")
+// var b=a*1
+// var m=c*1
+// var k=p*1
+
+// var n=b%2
+// var o=m%2
+// var t=k%2
+// if(n==0 && o==0 || o==0 && t==0 || n==o && t==0){
+//     console.log("Bu true")
+// }else (
+//     console.log("Bu false")
+// )
+
+
+
+
+// // 10-misol
+
+
+
+
+// var a=prompt("Iltimos hafta kunlarining raqamini kiriting bu 10-misol")
+// var b=a*1
+// if(b==1){
+//     console.log("Bu Dushanba")
+// }else if(b==2){
+//     console.log("Bu Seshanba")
+// }else if(b==3){
+//     console.log("Bu Chorshanba")
+// }else if(b==4){
+//     console.log("Bu Payshanba")
+// }else if(b==5){
+//     console.log("Bu Juma")
+// }else if(b==6){
+//     console.log("Bu Shanba")
+// }else if(b==7){
+//     console.log("Bu Yakshanba")
+// }else{
+//     console.log("Iltimos faqat 7tigacha bolgan RAQAM kiriting")
+// }
+
+
+
+
+// // 11-misol
+
+// var kod=prompt("telefon raqamingizning kampaniya kodini kiriting")
+// var kodd=kod*1
+// if(kodd==94||kodd==93){
+//     console.log("Bu Ucell")
+// }else if(kodd==90||kodd==91){
+//     console.log("Bu Beeline")
+// }else if(kodd==95){
+//     console.log("Bu Uzmobile yoki Uztelecom")
+// }else if(kodd==33){
+//     console.log("Bu Humans")
+// }else{
+//     console.log("Buni bilmayman")
+// }
+
+
+
+// // 12-misol
+
+
+// var a = prompt("butun son kiriting")
+// var b=a*1
+// var c=b%2
+// if(c==0){
+//     console.log(b+1)
+// }else{
+//     console.log(b-1)
+// }
+
+
+
+// // 13-misol
+
+
+// var a = prompt("butun son kiriting")
+// var b=a*1
+// var c=b%2
+// if(c==0){
+//     console.log(b+3)
+// }else{
+//     console.log(b-2)
+// }
+
+
+
+
+
+
+// // 14-misol
+
+
+// var a = prompt("1-butun sonni kiriting")
+// var c = prompt("2-butun sonni kiriting")
+// var b=a*1
+// var d=c*1
+// if(b>d){
+//     console.log("1-son katta")
+// }else{
+//     console.log("2-son katta")
+// }
+
+
+
+// // 15-misol
+
+
+// var a = prompt("1-butun sonni kiriting")
+// var c = prompt("2-butun sonni kiriting")
+// var v = prompt("3-butun sonni kiriting")
+// var b=a*1
+// var d=c*1
+// var g=v*1
+// if(b>d && b>g){
+//     console.log("1-son katta")
+// }else if(d>b && d>g){
+//     console.log("2-son katta")
+// }else if(g>b && g>d){
+//     console.log("3-son katta")
+// }else{
+//     console.log("buni bilmadim brat")
+// }
+
+
+
+// // 16-misol
+
+
+// var a = prompt("1-butun sonni kiriting")
+// var c = prompt("2-butun sonni kiriting")
+// var v = prompt("3-butun sonni kiriting")
+// var b=a*1
+// var d=c*1
+// var g=v*1
+// if(b<d && b<g){
+//     console.log("1-son kichik")
+// }else if(d<b && d<g){
+//     console.log("2-son kichik")
+// }else if(g<b && g<d){
+//     console.log("3-son kichik")
+// }else{
+//     console.log("buni bilmadim brat")
+// }
